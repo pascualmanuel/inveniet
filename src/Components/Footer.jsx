14c113ccea69a337175b37307b2357c0e3ff36ca
pascualmanuel <manuelnpascual@gmail.com>
2024-09-11 17:47:57 -0300

    Finish front skeleton mobile

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,24 +5,26 @@ const Footer = () => {
   return (
     <>
       <div className="bg-iGrey  w-[100vw] ">
-        <div className="center max-w-[570px]">
-          <ReactSVG src={Logo} />
-          <p>
-            Somos la solución integrada para la grabación y gestión audiovisual
-            de procesos orales judiciales, diseñada para simplificar y asegurar
-            el manejo de información legal.
-          </p>
-          <li>Contacto</li>
-          <li>Preguntas frecuentes</li>
-          <li>Equipamiento</li>
-          <li>Nosotros</li>
+        <div className="center max-w-[570px] ">
+          <div className="py-10">
+            <ReactSVG className="mb-6" src={Logo} />
+            <p className="mb-8">
+              Somos la solución integrada para la grabación y gestión
+              audiovisual de procesos orales judiciales, diseñada para
+              simplificar y asegurar el manejo de información legal.
+            </p>
+            <li className="myPBold">Contacto</li>
+            <li className="myPBold my-1.5">Preguntas frecuentes</li>
+            <li className="myPBold">Equipamiento</li>
+            <li className="myPBold mt-1.5 mb-8">Nosotros</li>
 
-          <li className="pl-6 text-[#8D9098]">
-            11 de septiembre 4717, 4° Piso, CABA, Buenos Aires, Argentina
-          </li>
-          <li className="pl-6 text-[#8D9098]">Lunes - Viernes: 8 am - 6 pm</li>
-          <li className="pl-6 text-[#8D9098]">contacto@inveniet.com</li>
-          <li className="pl-6 text-[#8D9098]">(+52) 5555 147914</li>
+            <li className=" myP2">
+              11 de septiembre 4717, 4° Piso, CABA, Buenos Aires, Argentina
+            </li>
+            <li className=" myP2 my-1.5">Lunes - Viernes: 8 am - 6 pm</li>
+            <li className=" myP2">contacto@inveniet.com</li>
+            <li className=" myP2 mt-1.5">(+52) 5555 147914</li>
+          </div>
         </div>
       </div>
     </>
