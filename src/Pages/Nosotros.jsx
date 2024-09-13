8972e660fc11839db5f0cd5e23804f8c7c829475
pascualmanuel <manuelnpascual@gmail.com>
2024-09-13 16:48:22 -0300

    Equipment ok:)

--- src/Pages/Nosotros.jsx
+++ src/Pages/Nosotros.jsx
@@ -4,8 +4,27 @@ const Nosotros = () => {
   return (
     <>
       <div className="w-screen h-[250px] nosotros-bg lg:h-[350px] relative">
-        <p className="myH1 absolute text-white top-[120px] left-[25px]">
+        <p className="myH1 absolute text-white top-[190px] left-[25px] lg:left-[80px]">
           Nosotros
+        </p>
+      </div>
+      <div className="center max-w-[1077px]" style={{ marginBottom: "80px" }}>
+        <h3 className="myH2 mt-[40px] lg:mt-[115px] mb-[33px] max-w-[670px]">
+          Nuestra misión es
+          <span className="text-iLBlue">
+            &nbsp; modernizar y agilizar los procesos judiciales
+          </span>
+        </h3>
+        <p className="subH ">
+          Inveniet nació de la necesidad de modernizar y optimizar la gestión de
+          procesos orales judiciales en un mundo cada vez más digitalizado.
+          Fundada por un equipo de expertos en tecnología y derecho, nuestra
+          empresa se dedica a ofrecer soluciones innovadoras que faciliten la
+          grabación, almacenamiento y gestión de registros audiovisuales en
+          procedimientos legales. Desde nuestros inicios, desarrollamos Inveniet
+          en colaboración directa con profesionales del sector judicial para
+          crear una plataforma que no solo sea poderosa, sino también intuitiva
+          y fácil de usar.
         </p>
       </div>
     </>
