b790140483af7eac4d6783b84429f0717cd1bb09
pascualmanuel <manuelnpascual@gmail.com>
2024-10-08 11:30:41 -0300

    Rayitas

--- src/Components/EcgAnimation.jsx
+++ src/Components/EcgAnimation.jsx
@@ -1,158 +1,18 @@
-// import React, { useEffect, useState } from "react";
-// import { motion } from "framer-motion";
-
-// const BarAnimation = () => {
-//   const [barsCount, setBarsCount] = useState(0);
-//   const barMargin = 16; // Margen entre las barras en px
-
-//   useEffect(() => {
-//     const updateBarsCount = () => {
-//       const totalWidth = window.innerWidth; // Ancho total de la ventana
-//       const barWidth = 3; // Ancho de cada barra (strokeWidth)
-//       const marginWidth = barMargin; // Margen entre barras
-
-//       // Calcular la cantidad de barras que pueden caber
-//       const count = Math.floor(
-//         (totalWidth + marginWidth) / (barWidth + marginWidth)
-//       );
-//       setBarsCount(count);
-//     };
-
-//     // Inicializar el conteo de barras
-//     updateBarsCount();
-
-//     // Actualizar el conteo de barras al redimensionar la ventana
-//     window.addEventListener("resize", updateBarsCount);
-
-//     return () => {
-//       window.removeEventListener("resize", updateBarsCount);
-//     };
-//   }, [barMargin]);
-
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         justifyContent: "space-between", // Distribuir las barras
-//         alignItems: "flex-end",
-//         height: "200px",
-//         width: "100vw", // Asegurarse de que el contenedor tenga 100vw
-//         overflow: "hidden",
-//         position: "relative", // Para manejar la posición de las barras
-//       }}
-//     >
-//       {Array.from({ length: barsCount }).map((_, index) => {
-//         // Generar una altura aleatoria para cada barra
-//         const initialHeight = Math.random() * 150 + 50; // Altura aleatoria inicial
-//         return (
-//           <motion.div
-//             key={index}
-//             style={{
-//               width: "3px", // strokeWidth
-//               backgroundColor: "#ccc", // color de la barra
-//               height: initialHeight, // altura inicial aleatoria
-//               borderRadius: "8px", // borderRadius para las barras
-//             }}
-//             animate={{
-//               height: [initialHeight, Math.random() * 150 + 50, initialHeight], // Animar la altura de la barra
-//             }}
-//             transition={{
-//               duration: 3,
-//               repeat: Infinity,
-//               ease: "easeInOut",
-//               //   delay: index * 0.2, // Retraso para cada barra basado en su índice
-//             }}
-//           />
-//         );
-//       })}
-//     </div>
-//   );
-// };
-
-// export default BarAnimation;
-
-// import React, { useEffect, useState } from "react";
-// import { motion } from "framer-motion";
-
-// const BarAnimation = () => {
-//   const [barsCount, setBarsCount] = useState(0);
-//   const barMargin = 16; // Margen entre las barras en px
-
-//   useEffect(() => {
-//     const updateBarsCount = () => {
-//       const totalWidth = window.innerWidth; // Ancho total de la ventana
-//       const barWidth = 3; // Ancho de cada barra (strokeWidth)
-//       const marginWidth = barMargin; // Margen entre barras
-
-//       // Calcular la cantidad de barras que pueden caber
-//       const count = Math.floor(
-//         (totalWidth + marginWidth) / (barWidth + marginWidth)
-//       );
-//       setBarsCount(count);
-//     };
-
-//     // Inicializar el conteo de barras
-//     updateBarsCount();
-
-//     // Actualizar el conteo de barras al redimensionar la ventana
-//     window.addEventListener("resize", updateBarsCount);
-
-//     return () => {
-//       window.removeEventListener("resize", updateBarsCount);
-//     };
-//   }, [barMargin]);
-
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         justifyContent: "space-between", // Distribuir las barras
-//         alignItems: "flex-end",
-//         height: "200px",
-//         width: "100vw", // Asegurarse de que el contenedor tenga 100vw
-//         overflow: "hidden",
-//         position: "relative", // Para manejar la posición de las barras
-//       }}
-//     >
-//       {Array.from({ length: barsCount }).map((_, index) => (
-//         <motion.div
-//           key={index}
-//           style={{
-//             width: "3px", // strokeWidth
-//             backgroundColor: "#ccc", // color de la barra
-//             height: "100px", // altura inicial
-//             borderRadius: "8px", // borderRadius para las barras
-//           }}
-//           animate={{
-//             height: [50, Math.random() * 150 + 50, 50], // Animar la altura de la barra
-//           }}
-//           transition={{
-//             duration: 2,
-//             repeat: Infinity,
-//             ease: "easeInOut",
-//             delay: index * 0.2, // Retraso para cada barra basado en su índice
-//           }}
-//         />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default BarAnimation;
-
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 const BarAnimation = () => {
   const [barsCount, setBarsCount] = useState(0);
   const [delays, setDelays] = useState([]);
-  const barMargin = 16;
+  const [isMobile, setIsMobile] = useState(false); // Para detectar si es mobile
 
   useEffect(() => {
     const updateBarsCount = () => {
       const totalWidth = window.innerWidth; // Ancho total de la ventana
-      const barWidth = 3;
-      const marginWidth = barMargin;
+
+      // Ajustes según el tamaño de pantalla
+      const barWidth = isMobile ? 2 : 3; // Ancho de barra: 2px en mobile, 3px en desktop
+      const marginWidth = isMobile ? 9 : 16; // Espacio entre barras: 6.5px en mobile, 16px en desktop
 
       // Calcular la cantidad de barras que pueden caber
       const count = Math.floor(
@@ -170,7 +30,9 @@ const BarAnimation = () => {
 
     const handleResize = () => {
       const currentWidth = window.innerWidth;
-      const currentHeight = window.innerHeight;
+
+      // Detectar si es mobile
+      setIsMobile(currentWidth < 768); // Consideramos "mobile" si el ancho es menor a 768px
 
       // Si solo cambia la altura, no hacemos nada
       if (currentWidth === window.previousWidth) return;
@@ -181,6 +43,7 @@ const BarAnimation = () => {
     };
 
     window.previousWidth = window.innerWidth; // Inicializamos con el ancho actual
+    setIsMobile(window.innerWidth < 768); // Comprobamos al cargar si es mobile
     window.addEventListener("resize", handleResize);
 
     // Llamamos la función inicial para configurar las barras al cargar
@@ -189,7 +52,7 @@ const BarAnimation = () => {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, [barMargin]);
+  }, [isMobile]);
 
   return (
     <div
